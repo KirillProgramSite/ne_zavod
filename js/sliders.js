5d3b1652afef6feea9348db72e5ad4fc4568c92e
Kirill Proskurin <78263182+KirillProgramSite@users.noreply.github.com>
2024-02-12 01:10:44 +0300

    Add files via upload

---
+++ js/sliders.js
@@ -0,0 +1,59 @@
+const swiper = new Swiper('.lines__slider', {
+  breakpoints: {
+    450:{
+      // centeredSlides: false,
+      slidesPerView: 4,
+      slidesPerGroup: 16,
+    },
+
+    
+  },
+  slidesPerView: 1,
+  slidesPerGroup: 16,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.button-next',
+      prevEl: '.button-prev',
+    },
+    speed: 1000,
+});
+
+
+const swiperSlider3d = new Swiper(".slider3d", {
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: false
+  },
+  speed: 100,
+  allowTouchMove: false,
+  navigation: {
+    nextEl: ".modal-next",
+    prevEl: ".modal-prev",
+  },
+});
+
+const swiperLidersSlider = new Swiper(".lidersslider", {
+  breakpoints: {
+    450:{
+      centeredSlides: true,
+      slidesPerView: 1.45,
+    },
+  },
+
+
+  initialSlide: 2,
+  loop: true,
+  centeredSlides: false,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.lider-next',
+    prevEl: '.lider-prev',
+  },
+  speed: 2000,
+});
+
+
+
+// swiper.controller.control = swiperSlider3d;
+swiperSlider3d.controller.control = swiper;
